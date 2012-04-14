670938ea4841709dc81d6a5fee01a2f4dda03f9f
FND <fnd@fnd-oda.localdomain>
2012-04-14 21:53:13 +0200

    fixed error reporting in tests
    as pointed out by @cdent, assertions should output the response body
    when an error occurs
    
    This reverts commit 3ea4a79ba392cf2c259377893dc6a045ea0ce54f.

--- test/test_basics.py
+++ test/test_basics.py
@@ -5,7 +5,6 @@ from pytest import mark
 
 from tiddlyweb.config import config
 from tiddlyweb.web import serve
-from tiddlyweb.web.http import HTTPExceptor
 
 
 HOSTNAME = "0.0.0.0"
@@ -16,9 +15,6 @@ HOST = "http://%s:%s" % (HOSTNAME, PORT)
 def setup_module(module):
     module.environ = { "tiddlyweb.config": config }
     config["system_plugins"].append("tiddlywebplugins.webdav")
-    # remove HTTPExceptor to avoid hiding exception details
-    config["server_response_filters"] = [item for item
-        in config["server_response_filters"] if not item == HTTPExceptor]
 
     httplib2_intercept.install()
     add_wsgi_intercept(HOSTNAME, PORT, _app)
@@ -30,20 +26,20 @@ def setup_module(module):
 def test_nondestructive():
     # ensure original root is not overridden
     response, content = client.request("%s/" % HOST, "GET")
-    assert response["status"] == "200"
+    assert response["status"] == "200", content
 
 
 def test_handshake():
     response, content = client.request("%s/" % HOST, "OPTIONS")
-    assert response["status"] == "200"
-    assert response["dav"] == "1"
-    assert response["ms-author-via"] == "DAV"
+    assert response["status"] == "200", content
+    assert response["dav"] == "1", content
+    assert response["ms-author-via"] == "DAV", content
 
 
 def test_directory_listing():
     response, content = client.request("%s/" % HOST, "PROPFIND")
-    assert response["status"] == "207"
-    assert response["content-type"] == "application/xml"
+    assert response["status"] == "207", content
+    assert response["content-type"] == "application/xml", content
     assert "<?xml " in content
 
 
