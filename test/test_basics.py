3ea4a79ba392cf2c259377893dc6a045ea0ce54f
FND <fnd@fnd-oda.localdomain>
2012-04-14 21:17:12 +0200

    removed HTTPExceptor for testing to avoid hiding exception details
    this seems awkward; a more generic solution might be desirable

--- test/test_basics.py
+++ test/test_basics.py
@@ -5,6 +5,7 @@ from pytest import mark
 
 from tiddlyweb.config import config
 from tiddlyweb.web import serve
+from tiddlyweb.web.http import HTTPExceptor
 
 
 HOSTNAME = "0.0.0.0"
@@ -15,6 +16,9 @@ HOST = "http://%s:%s" % (HOSTNAME, PORT)
 def setup_module(module):
     module.environ = { "tiddlyweb.config": config }
     config["system_plugins"].append("tiddlywebplugins.webdav")
+    # remove HTTPExceptor to avoid hiding exception details
+    config["server_response_filters"] = [item for item
+        in config["server_response_filters"] if not item == HTTPExceptor]
 
     httplib2_intercept.install()
     add_wsgi_intercept(HOSTNAME, PORT, _app)
